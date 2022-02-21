4174fffb1ba3954416bb7dbe8daf21403abe8229
Yevhenii Turianskyi <zhenya5990@gmail.com>
2022-02-21 12:13:30 +0200

    1

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -22,7 +22,13 @@ let text =
 
 bot.command('add', async (ctx: any) => {
   if (JSON.parse('[' + process.env.ADMIN_IDS + ']').includes(ctx.from.id)) {
-    console.log(ctx.chat);
+    console.log(
+      ctx.message,
+      ctx.messageId,
+      ctx.message_id,
+      ctx.inline_message_id,
+      ctx.inlineMessageId(),
+    );
     // ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.id);
     ctx.telegram.sendMessage(ctx.chat.id, text, { parse_mode: 'html' });
   } else {
